4b8ac6a98b3a54722fc821aaaacf8e2a5f2b3437
GDJ26 <GDJ26@DESKTOP-TBTANMQ>
2020-05-12 18:00:57 +0900

    index.html 폼 작성 , MemberController 작성, MemberMapper 작성, MemberMapper.xml 작성 MemberService 작성

---
+++ cashbook/src/main/java/com/gdu/cashbook/controller/MemberController.java
@@ -0,0 +1,27 @@
+package com.gdu.cashbook.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.gdu.cashbook.service.MemberService;
+import com.gdu.cashbook.vo.Member;
+
+@Controller
+public class MemberController {
+	@Autowired
+	private MemberService memberService;
+	
+	@GetMapping("/addMember")
+	public String addMember() {
+		return "addMember";
+	}
+	
+	@PostMapping("/addMember")
+	public String addMember(Member member) {
+		memberService.addMember(member);
+		System.out.println(member);
+		return "redirect:/index";
+	}
+}
